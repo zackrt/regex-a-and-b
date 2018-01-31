8cff60f5f917e36f6a6c067db8c94dca371a0c7c
Zack Toth <zacharyrtoth@gmail.com>
2018-01-30 16:08:59 -0800

    added js file

---
+++ regexAB.js
@@ -0,0 +1,22 @@
+
+// ChallengesLearn ⌄ProfilePremium Membership
+
+// Tags: string manipulation, regular expression | Difficulty: Easy
+// Challenge
+// Using the JavaScript language, have the function ABCheck(str) take the str parameter being passed and return the string true if the characters a and b are separated by exactly 3 places anywhere in the string at least once (ie. "lane borrowed" would result in true because there is exactly three characters between a and b). Otherwise return the string false. 
+
+// Sample Test Cases
+// Input:"after badly"
+// Output:"false"
+
+// Input:"Laura sobs"
+// Output:"true"
+
+function ABCheck(str) {
+  
+  
+  
+}
+
+console.log(ABCheck("after badly"))
+console.log(ABCheck("Laura sobs"))
